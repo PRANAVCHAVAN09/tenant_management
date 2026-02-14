ff40b9e86cce48515324eb3a5740d6622390583e
PRANAVCHAVAN09 <pranavchavan166@gmail.com>
2026-02-14 17:05:34 +0530

    env added cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,11 +14,22 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 // app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://*.vercel.app"
-  ],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }    if (origin.endsWith(".vercel.app")) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
   credentials: true
 }));
 
